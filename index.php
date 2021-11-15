cdb969a820162f2785f261988789bafad54e1819
Jacques van Wyk <jvw679@gmail.com>
2021-11-15 20:33:54 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+/**
+* Plugin Name:       Motion Stack Real Estate Plugin
+* Plugin URI:        https://motionstack.design/plugins/real-eastate
+* Description:       Real Estate Wordpress Plugin
+* Version:           0.0.1
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            Jacques van Wyk
+* Author URI:        https://motionstack.design/
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI:        https://example.com/my-plugin/
+* Text Domain:       motionstack-real-estate
